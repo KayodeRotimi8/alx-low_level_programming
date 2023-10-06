5a0833b3d60c1df4c1b6bfbc833c778213f1ad98
KayodeRotimi8 <kayodejohn20@gmail.com>
2023-10-05 19:50:33 -0700

    C program that prints ouput followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints and that piece of art is useful
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
