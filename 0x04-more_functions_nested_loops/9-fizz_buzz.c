38a94f8dca00c64685dbd061b9e1cb8ae720af8a
KayodeRotimi8 <kayodejohn20@gmail.com>
2023-10-12 14:37:56 -0700

    The “Fizz-Buzz test”

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int multiples = 1;
+
+	while (multiples <= 100)
+	{
+		if (multiples % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (multiples % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (multiples % 3 == 0 && multiples % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", multiples);
+		}
+		if (multiples != 100)
+		{
+			printf(" ");
+		}
+		multiples++;
+	}
+	printf("\n");
+	return (0);
+}
