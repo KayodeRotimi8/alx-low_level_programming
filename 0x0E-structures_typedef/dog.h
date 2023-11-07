df6f81c42d107806121e58eb7f1dfc833cf01f47
KayodeRotimi8 <kayodejohn20@gmail.com>
2023-11-06 20:22:37 -0800

    defining a new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - a dog structure
+ * @name: pointer to character storing name of dog
+ * @age: float representing age of dog
+ * @owner: pointer to character storing name of dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
